32bf364410d08af1f1af56a37be8bc0c1a030c55
cucmberium <cucmberium@clarkdale.sakura.ne.jp>
2023-09-17 21:51:33 +0900

    Reorder using of objects

--- TootNet/Objects/Poll.cs
+++ TootNet/Objects/Poll.cs
@@ -1,6 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using TootNet.Internal;
 
 namespace TootNet.Objects
--- TootNet/Objects/Relationship.cs
+++ TootNet/Objects/Relationship.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.ComponentModel;
-using Newtonsoft.Json;
 using TootNet.Internal;
 
 namespace TootNet.Objects
