89193255034d3d5eea3a50b10bdc8df588fec6a6
cucmberium <cucmberium@clarkdale.sakura.ne.jp>
2023-09-17 23:39:55 +0900

    Update streaming apis for latest mastodon

--- TootNet/Streaming/Streaming.cs
+++ TootNet/Streaming/Streaming.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using TootNet.Internal;
 using TootNet.Objects;
@@ -13,9 +14,14 @@ namespace TootNet.Streaming
     public enum StreamingType
     {
         User = 0,
-        Hashtag = 1,
+        UserNotification = 1,
         Public = 2,
-        List = 3
+        PublicLocal = 3,
+        PublicRemote = 4,
+        Hashtag = 5,
+        HashtagLocal = 6,
+        List = 7,
+        Direct = 8
     }
 
     public class StreamingApi : ApiBase
@@ -23,55 +29,28 @@ namespace TootNet.Streaming
         internal StreamingApi(Tokens e) : base(e) { }
 
         /// <summary>
-        /// Streams messages for a hashtag timeline.
+        /// <para>Check if the server is alive</para>
         /// <para>Available parameters:</para>
-        /// <para>- <c>string</c> tag (required)</para>
+        /// <para>- No parameters available in this method.</para>
         /// </summary>
         /// <param name="parameters">The parameters.</param>
-        /// <returns>The stream messages.</returns>
-        public IObservable<StreamingMessage> HashtagAsObservable(params Expression<Func<string, object>>[] parameters)
+        /// <returns>
+        /// <para>The task object representing the asynchronous operation.</para>
+        /// <para>The Result property on the task object returns the string object.</para>
+        /// </returns>
+        public Task<string> CheckAsync(params Expression<Func<string, object>>[] parameters)
         {
-            return new StreamingObservable(Tokens, StreamingType.Hashtag, Utils.ExpressionToDictionary(parameters));
+            return Tokens.AccessApiAsync<string>(MethodType.Get, "streaming/health", Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="CheckAsync(Expression{Func{string, object}}[])"/>
+        public Task<string> CheckAsync(IDictionary<string, object> parameters)
+        {
+            return Tokens.AccessApiAsync<string>(MethodType.Get, "streaming/health", parameters);
         }
 
         /// <summary>
-        /// Streams messages for a hashtag timeline.
-        /// <para>Available parameters:</para>
-        /// <para>- <c>string</c> tag (required)</para>
-        /// </summary>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>The stream messages.</returns>
-        public IObservable<StreamingMessage> HashtagAsObservable(IDictionary<string, object> parameters)
-        {
-            return new StreamingObservable(Tokens, StreamingType.Hashtag, parameters);
-        }
-
-        /// <summary>
-        /// Streams messages for a hashtag timeline.
-        /// <para>Available parameters:</para>
-        /// <para>- <c>string</c> tag (required)</para>
-        /// </summary>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>The stream messages.</returns>
-        public IObservable<StreamingMessage> PublicAsObservable(params Expression<Func<string, object>>[] parameters)
-        {
-            return new StreamingObservable(Tokens, StreamingType.Public, Utils.ExpressionToDictionary(parameters));
-        }
-
-        /// <summary>
-        /// Streams messages for a public timeline.
-        /// <para>Available parameters:</para>
-        /// <para>- <c>bool</c> local (optional)</para>
-        /// </summary>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>The stream messages.</returns>
-        public IObservable<StreamingMessage> PublicAsObservable(IDictionary<string, object> parameters)
-        {
-            return new StreamingObservable(Tokens, StreamingType.Public, parameters);
-        }
-
-        /// <summary>
-        /// Streams messages for a single user.
+        /// Watch your home timeline and notifications
         /// <para>Available parameters:</para>
         /// <para>- No parameters available in this method.</para>
         /// </summary>
@@ -82,20 +61,122 @@ namespace TootNet.Streaming
             return new StreamingObservable(Tokens, StreamingType.User, Utils.ExpressionToDictionary(parameters));
         }
 
-        /// <summary>
-        /// Streams messages for a single user.
-        /// <para>Available parameters:</para>
-        /// <para>- No parameters available in this method.</para>
-        /// </summary>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>The stream messages.</returns>
+        /// <inheritdoc cref="UserAsObservable(Expression{Func{string, object}}[])"/>
         public IObservable<StreamingMessage> UserAsObservable(IDictionary<string, object> parameters)
         {
             return new StreamingObservable(Tokens, StreamingType.User, parameters);
         }
 
         /// <summary>
-        /// Streams messages for a list timeline.
+        /// Watch your notifications
+        /// <para>Available parameters:</para>
+        /// <para>- No parameters available in this method.</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> UserNotificationAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.UserNotification, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="UserNotificationAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> UserNotificationAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.UserNotification, parameters);
+        }
+
+        /// <summary>
+        /// Watch the federated timeline
+        /// <para>Available parameters:</para>
+        /// <para>- <c>bool</c> only_media (optional)</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> PublicAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.Public, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="PublicAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> PublicAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.Public, parameters);
+        }
+
+        /// <summary>
+        /// Watch the local timeline
+        /// <para>Available parameters:</para>
+        /// <para>- <c>bool</c> only_media (optional)</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> PublicLocalAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.PublicLocal, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="PublicLocalAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> PublicLocalAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.PublicLocal, parameters);
+        }
+
+        /// <summary>
+        /// Watch the remote statuses
+        /// <para>Available parameters:</para>
+        /// <para>- <c>bool</c> only_media (optional)</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> PublicRemoteAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.PublicRemote, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="PublicRemoteAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> PublicRemoteAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.PublicRemote, parameters);
+        }
+
+        /// <summary>
+        /// Watch the public timeline for a hashtag
+        /// <para>Available parameters:</para>
+        /// <para>- <c>string</c> tag (required)</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> HashtagAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.Hashtag, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="HashtagAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> HashtagAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.Hashtag, parameters);
+        }
+
+        /// <summary>
+        /// Watch the local timeline for a hashtag
+        /// <para>Available parameters:</para>
+        /// <para>- <c>string</c> tag (required)</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> HashtagLocalAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.HashtagLocal, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="HashtagLocalAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> HashtagLocalAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.HashtagLocal, parameters);
+        }
+
+        /// <summary>
+        /// Watch for list updates
         /// <para>Available parameters:</para>
         /// <para>- <c>long</c> list (required)</para>
         /// </summary>
@@ -106,16 +187,28 @@ namespace TootNet.Streaming
             return new StreamingObservable(Tokens, StreamingType.List, Utils.ExpressionToDictionary(parameters));
         }
 
-        /// <summary>
-        /// Streams messages for a list timeline.
-        /// <para>Available parameters:</para>
-        /// <para>- <c>long</c> list (required)</para>
-        /// </summary>
-        /// <param name="parameters">The parameters.</param>
-        /// <returns>The stream messages.</returns>
+        /// <inheritdoc cref="ListAsObservable(Expression{Func{string, object}}[])"/>
         public IObservable<StreamingMessage> ListAsObservable(IDictionary<string, object> parameters)
         {
             return new StreamingObservable(Tokens, StreamingType.List, parameters);
+        }
+
+        /// <summary>
+        /// Watch for direct messages
+        /// <para>Available parameters:</para>
+        /// <para>- No parameters available in this method.</para>
+        /// </summary>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns>The stream messages.</returns>
+        public IObservable<StreamingMessage> DirectAsObservable(params Expression<Func<string, object>>[] parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.Direct, Utils.ExpressionToDictionary(parameters));
+        }
+
+        /// <inheritdoc cref="DirectAsObservable(Expression{Func{string, object}}[])"/>
+        public IObservable<StreamingMessage> DirectAsObservable(IDictionary<string, object> parameters)
+        {
+            return new StreamingObservable(Tokens, StreamingType.Direct, parameters);
         }
     }
 
@@ -135,33 +228,52 @@ namespace TootNet.Streaming
 
         public IDisposable Subscribe(IObserver<StreamingMessage> observer)
         {
-            var streamingUrl = _tokens.Instance;
             var conn = new StreamingConnection();
+
+            var url = "https://" + _tokens.Instance + "/api/v1/streaming";
             switch (_type)
             {
                 case StreamingType.User:
-                    conn.Start(observer, _tokens, "https://" + streamingUrl + "/api/v1/streaming/user");
+                    url += "/user";
+                    break;
+                case StreamingType.UserNotification:
+                    url += "/user/notification";
+                    break;
+                case StreamingType.Public:
+                    url += "/public";
+                    break;
+                case StreamingType.PublicLocal:
+                    url += "/public/local";
+                    break;
+                case StreamingType.PublicRemote:
+                    url += "/public/remote";
                     break;
                 case StreamingType.Hashtag:
                     if (!_parameters.ContainsKey("tag") || string.IsNullOrEmpty(_parameters["tag"].ToString()))
-                        throw new ArgumentException("You must specify a hashtag");
+                        throw new ArgumentException("You must specify a tag");
 
-                    conn.Start(observer, _tokens, "https://" + streamingUrl + "/api/v1/streaming/hashtag" + "?tag=" + _parameters["tag"]);
+                    url += "/hashtag";
                     break;
-                case StreamingType.Public:
-                    var publicStreamingUrl = "https://" + streamingUrl + "/api/v1/streaming/public";
-                    if (_parameters.ContainsKey("local") && (bool)_parameters["local"])
-                        publicStreamingUrl += "/local";
+                case StreamingType.HashtagLocal:
+                    if (!_parameters.ContainsKey("tag") || string.IsNullOrEmpty(_parameters["tag"].ToString()))
+                        throw new ArgumentException("You must specify a tag");
 
-                    conn.Start(observer, _tokens, publicStreamingUrl);
+                    url += "/hashtag/local";
                     break;
                 case StreamingType.List:
                     if (!_parameters.ContainsKey("list") || string.IsNullOrEmpty(_parameters["list"].ToString()))
-                        throw new ArgumentException("You must specify a list id");
+                        throw new ArgumentException("You must specify a list");
 
-                    conn.Start(observer, _tokens, "https://" + streamingUrl + "/api/v1/streaming/list" + "?list=" + _parameters["list"]);
+                    url += "/list";
+                    break;
+                case StreamingType.Direct:
+                    url += "/direct";
                     break;
             }
+
+            url = _parameters == null ? url : Utils.CreateUrlParameter(url, _parameters);
+
+            conn.Start(observer, _tokens, url);
             return conn;
         }
     }
@@ -204,18 +316,40 @@ namespace TootNet.Streaming
                                 {
                                     case "update":
                                         var status = JsonConvert.DeserializeObject<Status>(data);
-                                        observer.OnNext(new StreamingMessage(status));
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.Status, status));
+                                        break;
+                                    case "delete":
+                                        var deletedStatusId = long.Parse(data);
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.StatusDelete, deletedStatusId));
                                         break;
                                     case "notification":
                                         var notification = JsonConvert.DeserializeObject<Notification>(data);
-                                        observer.OnNext(new StreamingMessage(notification));
-                                        break;
-                                    case "delete":
-                                        var statusId = long.Parse(data);
-                                        observer.OnNext(new StreamingMessage(statusId));
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.Notification, notification));
                                         break;
                                     case "filters_changed":
                                         observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.FiltersChanged));
+                                        break;
+                                    case "conversation":
+                                        var conversation = JsonConvert.DeserializeObject<Conversation>(data);
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.Conversation, conversation));
+                                        break;
+                                    case "announcement":
+                                        var announcement = JsonConvert.DeserializeObject<Announcement>(data);
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.Announcement, announcement));
+                                        break;
+                                    case "announcement.reaction":
+                                        var announcementReaction = JsonConvert.DeserializeObject<Reaction>(data);
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.AnnouncementReaction, announcementReaction));
+                                        break;
+                                    case "announcement.delete":
+                                        var deletedAnnouncementId = long.Parse(data);
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.AnnouncementDelete, deletedAnnouncementId));
+                                        break;
+                                    case "status.update":
+                                        var updatedStatus = JsonConvert.DeserializeObject<Status>(data);
+                                        observer.OnNext(new StreamingMessage(StreamingMessage.MessageType.StatusUpdate, updatedStatus));
+                                        break;
+                                    case "encrypted_message":
                                         break;
                                 }
                             }
--- TootNet/Streaming/StreamingMessage.cs
+++ TootNet/Streaming/StreamingMessage.cs
@@ -1,4 +1,5 @@
-﻿using TootNet.Objects;
+﻿using System;
+using TootNet.Objects;
 
 namespace TootNet.Streaming
 {
@@ -6,28 +7,15 @@ namespace TootNet.Streaming
     {
         public enum MessageType
         {
-            DeleteStatus = 0,
-            Status = 1,
+            Status = 0,
+            StatusDelete = 1,
             Notification = 2,
-            FiltersChanged = 3
-        }
-        
-        public StreamingMessage(Status status)
-        {
-            Type = MessageType.Status;
-            Status = status;
-        }
-
-        public StreamingMessage(Notification notification)
-        {
-            Type = MessageType.Notification;
-            Notification = notification;
-        }
-
-        public StreamingMessage(long id)
-        {
-            Type = MessageType.DeleteStatus;
-            DeletedId = id;
+            FiltersChanged = 3,
+            Conversation = 4,
+            Announcement = 5,
+            AnnouncementReaction = 6,
+            AnnouncementDelete = 7,
+            StatusUpdate = 8,
         }
 
         public StreamingMessage(MessageType msgType)
@@ -35,10 +23,77 @@ namespace TootNet.Streaming
             Type = msgType;
         }
 
+        public StreamingMessage(MessageType msgType, Status status) : this(msgType)
+        {
+            switch (msgType)
+            {
+                case MessageType.Status:
+                    Status = status;
+                    break;
+                case MessageType.StatusUpdate:
+                    UpdatedStatus = status;
+                    break;
+            }
+
+            throw new ArgumentException("Invalid message type received");
+        }
+
+        public StreamingMessage(MessageType msgType, Notification notification) : this(msgType)
+        {
+            if (msgType != MessageType.Notification)
+                throw new ArgumentException("Invalid message type received");
+
+            Notification = notification;
+        }
+
+        public StreamingMessage(MessageType msgType, Conversation conversation) : this(msgType)
+        {
+            if (msgType != MessageType.Conversation)
+                throw new ArgumentException("Invalid message type received");
+
+            Conversation = conversation;
+        }
+
+        public StreamingMessage(MessageType msgType, Announcement announcement) : this(msgType)
+        {
+            if (msgType != MessageType.Announcement)
+                throw new ArgumentException("Invalid message type received");
+
+            Announcement = announcement;
+        }
+
+        public StreamingMessage(MessageType msgType, Reaction announcementReaction) : this(msgType)
+        {
+            if (msgType != MessageType.AnnouncementReaction)
+                throw new ArgumentException("Invalid message type received");
+
+            AnnouncementReaction = announcementReaction;
+        }
+
+        public StreamingMessage(MessageType msgType, long id) : this(msgType)
+        {
+            switch (msgType)
+            {
+                case MessageType.StatusDelete:
+                    DeletedStatusId = id;
+                    break;
+                case MessageType.AnnouncementDelete:
+                    DeletedAnnouncementId = id;
+                    break;
+            }
+
+            throw new ArgumentException("Invalid message type received");
+        }
+
         public MessageType Type { get; set; }
 
         public Status Status { get; set; }
+        public Status UpdatedStatus { get; set; }
+        public long? DeletedStatusId { get; set; }
         public Notification Notification { get; set; }
-        public long DeletedId { get; set; }
+        public Conversation Conversation { get; set; }
+        public Announcement Announcement { get; set; }
+        public Reaction AnnouncementReaction { get; set; }
+        public long? DeletedAnnouncementId { get; set; }
     }
 }
